70f528bc6db16feafb353b799644c41361042fe9
Dawitkidane <dh.kidane@stud.uis.no>
2018-02-23 09:35:13 +0100

    23.02.2018
    dawit

--- CityShare_Flask/CityShare.py
+++ CityShare_Flask/CityShare.py
@@ -429,14 +429,7 @@ def deleteShape():
 
     return "Deleted"
 
-<<<<<<< HEAD
-#comment dawit
-=======
-
-### comment mohammed
->>>>>>> c60f5a0f84299a8e4716c1c35f85de0d8f7ba9b4
 
 if __name__ == '__main__':
-
     app.run()
 
